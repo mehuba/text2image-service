935457a90d3797e609e85b594ebdedb94a34f4fb
zhaoyibin <zhaoyibin.rock@bytedance.com>
2025-06-02 00:37:26 +0800

    Fix: select

--- text2image-master/src/main/java/com/example/text2image/master/service/WorkerRegistryService.java
+++ text2image-master/src/main/java/com/example/text2image/master/service/WorkerRegistryService.java
@@ -1,7 +1,8 @@
 package com.example.text2image.master.service;
 
+import com.example.text2image.common.dto.TaskRequest;
 import com.example.text2image.common.dto.WorkerInfo;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class WorkerRegistryService {
     private final Map<String, WorkerInfo> workers = new ConcurrentHashMap<>();
+    private RestTemplate restTemplate = new RestTemplate();
 
     public void register(WorkerInfo info) {
         workers.put(info.getId(), info);
@@ -22,7 +24,12 @@ public class WorkerRegistryService {
 
         for (Map.Entry<String, WorkerInfo> worker : workers.entrySet()) {
             try {
-                ResponseEntity<Map> res = new RestTemplate().getForEntity(worker.getValue().getAddress() + "/gpu-usage", Map.class);
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                // 某些服务器默认拒绝非浏览器类请求, 必须加这个header否则报错403
+                headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+                HttpEntity<TaskRequest> entity = new HttpEntity<>(headers);
+                ResponseEntity<Map> res = restTemplate.exchange(worker.getValue().getAddress() + "/gpu-usage", HttpMethod.GET, entity, Map.class);
                 Integer usage = (Integer) res.getBody().get("usage");
                 if (usage < minUsage) {
                     minUsage = usage;
@@ -32,7 +39,7 @@ public class WorkerRegistryService {
 
             }
         }
-
+        System.out.println("Selected worker: " + selected);
         return selected; // 返回最优 worker URL
     }
 }
